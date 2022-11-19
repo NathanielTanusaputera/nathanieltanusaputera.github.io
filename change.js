fc825e05beee6159fef8ee1e6ae265f1d6bf106b
Legogundam13 <ntanusaputera@gmail.com>
2022-11-19 23:45:28 +0700

    a

---
+++ change.js
@@ -0,0 +1,54 @@
+function changeImage1(){
+    var img = document.getElementById('menu-drinks');
+    img.src="Layer 1.png";
+    var img = document.getElementById('gambar-drinks');
+    img.src="ewretgh.jpg"
+}
+function changeImage2(){
+    var img = document.getElementById('menu-drinks');
+    img.src="Layer 2.png";
+    var img = document.getElementById('gambar-drinks');
+    img.src="awrgrd.png"
+}
+function changeImage3(){
+    var img = document.getElementById('menu-drinks');
+    img.src="Layer 3.png";
+    var img = document.getElementById('gambar-drinks');
+    img.src="ewfrgfhjmh.jpg"
+}
+function changeImage4(){
+    var img = document.getElementById('menu-drinks');
+    img.src="Layer 4.png";
+    var img = document.getElementById('gambar-drinks');
+    img.src="srty.jpg"
+}
+function changeImage5(){
+    var img = document.getElementById('menu-drinks');
+    img.src="Layer 5.png";
+    var img = document.getElementById('gambar-drinks');
+    img.src="waesrdgtfhygj.jpg"
+}
+function changeImage6(){
+    var img = document.getElementById('menu-drinks');
+    img.src="Layer 6.png";
+    var img = document.getElementById('gambar-drinks');
+    img.src="sdfghjko.jpg"
+}
+function changeImage7(){
+    var img = document.getElementById('menu-drinks');
+    img.src="Layer 7.png";
+    var img = document.getElementById('gambar-drinks');
+    img.src="mie.png"
+}
+function changeImage8(){
+    var img = document.getElementById('menu-drinks');
+    img.src="Layer 8.png";
+    var img = document.getElementById('gambar-drinks');
+    img.src="odeng.png"
+}
+function changeImage9(){
+    var img = document.getElementById('menu-drinks');
+    img.src="Layer 9.png";
+    var img = document.getElementById('gambar-drinks');
+    img.src="arstdyfjgkh.png"
+}
